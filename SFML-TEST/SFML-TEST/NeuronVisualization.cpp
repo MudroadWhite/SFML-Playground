c695035fbb4847c947ee62d741c6f1f4c2077f49
MudroadWhite <53061787+MudroadWhite@users.noreply.github.com>
2023-02-23 05:06:05 -0600

    Update NeuronVisualization.cpp

--- SFML-TEST/SFML-TEST/NeuronVisualization.cpp
+++ SFML-TEST/SFML-TEST/NeuronVisualization.cpp
@@ -1,5 +1,26 @@
 #include "NeuronVisualization.h"
 
+// Visualization for perceptrons and simple layer NNs
+
+// Parameters:
+// 1. neuron size as a circle
+// 2. breathe mode?
+// 3. vectors of neurons
+
+// Class related:
+// NV has NVNeuron (Visual)
+// NV has NVNetwork (Model / real training)
+// NV has NVControl (Control)
+// NV has NVLib
+
+// TODO:
+// 1. draw neurons (NVNeuron)
+// 2. organize neurons so that they are aligned well (NVNeuron)
+// 3. make neurons breathe (NVNeuron)
+// 4. implement neuron related algorithms? (NVNetwork)
+// 5. make the update step-wise (NVControl)
+// 6. Finally, possibly add a mouse button and more other functionalities
+
 NeuronVisualization::NeuronVisualization()
 {
 }
